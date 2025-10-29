ac2566158a4655a95a84372afc3510f21e95ce94
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-10-29 18:57:34 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Footer from "./sections/Footer";
 
 const App = () => {
   function downloadCV() {
-    document.getElementById("id").href = "/assets/cv-oct-2025.pdf";
+    document.getElementById("id").href = "/public/assets/cv-oct-2025.pdf";
   }
 
   return (
@@ -112,28 +112,32 @@ const App = () => {
               title: "CV",
               href: "/assets/cristianharders-25-blue.pdf",
               icon: (
-                <svg
-                  onClick={downloadCV}
-                  id="cv"
-                  class="w-6 h-6 text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
+                <a
+                  href="/assets/cristianharders-25-blue.pdf"
+                  download
+                  className="flex items-center"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
-                    clip-rule="evenodd"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white hover:text-[#ed1313] transition-colors"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
+                      clipRule="evenodd"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
               ),
             },
           ]}
